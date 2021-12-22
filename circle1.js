c27058b65ff8e0c7b624ecf301113a9b6f7fe4ca
Kiu-Kyu <minkiukiu@gmail.com>
2021-12-22 15:19:06 +0700

    add 1 html file + 1 js file

---
+++ circle1.js
@@ -0,0 +1,15 @@
+class Circle {
+    radius;
+    color;
+
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    getRadius() {
+        return this.radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
